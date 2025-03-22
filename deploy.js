6ca38d4b4e5fff2e7f4809ed20d155b018be69d5
dvDaniel <113447778+devdaniel28@users.noreply.github.com>
2025-03-22 20:08:30 -0300

    agora vai

--- deploy.js
+++ deploy.js
@@ -37,7 +37,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
     try {
         console.log('🔄 Registrando Slash Commands no Servidor...');
         await rest.put(
-            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands }
         );
         console.log('✅ Slash Commands registrados no servidor!');
